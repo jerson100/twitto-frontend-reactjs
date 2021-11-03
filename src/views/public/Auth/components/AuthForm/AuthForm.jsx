83cb5714d33d28f15162565e24418b9ffd39fb52
jerson100 <juamkoo@gmail.com>
2021-11-03 15:10:56 -0500

    Agregando el Logo de la página

--- src/views/public/Auth/components/AuthForm/AuthForm.jsx
+++ src/views/public/Auth/components/AuthForm/AuthForm.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Paragraph from "../../../../../components/common/Paragraph";
 
 const AuthForm = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Paragraph as="p" type="h4">
+        Únete a Twitto hoy mismo
+      </Paragraph>
+    </div>
+  );
 };
 
 export default AuthForm;
--- src/views/public/Auth/components/AuthWrapper/authWrapper.style.js
+++ src/views/public/Auth/components/AuthWrapper/authWrapper.style.js
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 
 const AuthWrapperStyle = styled.div`
-  border: solid 1px red;
+  /* border: solid 1px red; */
   padding: 1rem;
 `;
 
 const AuthLogoStyle = styled.svg`
+  height: 2.7rem;
+  margin-bottom: 2rem;
   fill: ${({ theme }) => theme.COLORS.PRIMARY};
 `;
 
