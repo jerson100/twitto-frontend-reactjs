8f6abf71e1c773d01621e78e7cd6c6bc5b2dcefc
jerson100 <juamkoo@gmail.com>
2022-04-20 01:07:00 -0500

    1. Se agregó la función handleClick para poner navegar a la página en la que se pueda acceder al sistema twitto

--- src/views/public/Auth/components/AuthLoginButton/AuthLoginButton.jsx
+++ src/views/public/Auth/components/AuthLoginButton/AuthLoginButton.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "../../../../../components/common/Button";
 import Paragraph from "../../../../../components/common/Paragraph";
 
 const AuthLoginButton = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/i/flow/login");
+  };
   return (
     <>
       <Paragraph as="p" type="h6">
@@ -13,6 +18,7 @@ const AuthLoginButton = () => {
         size="NORMAL"
         color="BLUE"
         type="GOSTH"
+        handleClick={handleClick}
         //   maxWidth="300px"
       >
         Iniciar sesión
