253bc70a54480fb7da95b93300c27b0f66473c86
jerson100 <juamkoo@gmail.com>
2022-05-20 18:48:34 -0500

    Agregando el overflow para que el contenido que salga del contenedor se genere un scroll

--- src/components/layouts/PrivateMainLayout/components/Main/main.style.js
+++ src/components/layouts/PrivateMainLayout/components/Main/main.style.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
 const MainStyle = styled.main`
-  flex: 1 1 auto;
+  flex: 1 0 auto;
   border: solid 1px green;
+  overflow-y: auto;
 `;
 
 export { MainStyle };
