89a76742d83b920995e1e6aceeb9a04034edc0e0
jerson100 <juamkoo@gmail.com>
2022-05-21 02:00:17 -0500

    Agregando m{as item a la lista de accesibilidad

--- src/views/private/Settings/views/AccessibilityDispLg/AccessibilityDispLg.jsx
+++ src/views/private/Settings/views/AccessibilityDispLg/AccessibilityDispLg.jsx
@@ -32,6 +32,43 @@ const AccessibilityDispLg = () => {
             limitar el contraste de colores y el movimiento.
           </Paragraph>
         </List.Item>
+        <List.Item
+          to="/settings/accessibility_display_and_languages"
+          iconLeft={<Icon svg="closedEyes" size="SMALLER" color="SECONDARY" />}
+        >
+          <Paragraph ass="p" type="h5" marginBottom={false}>
+            Mostrar
+          </Paragraph>
+          <Paragraph color="SECONDARY" ass="p" type="h6" marginBottom={false}>
+            Administra el fondo, color y tamaño de la fuente. Esta configuración
+            afecta a todas las cuentas de Twitter en este navegador.
+          </Paragraph>
+        </List.Item>
+        <List.Item
+          to="/settings/accessibility_display_and_languages"
+          iconLeft={<Icon svg="closedEyes" size="SMALLER" color="SECONDARY" />}
+        >
+          <Paragraph ass="p" type="h5" marginBottom={false}>
+            Idiomas
+          </Paragraph>
+          <Paragraph color="SECONDARY" ass="p" type="h6" marginBottom={false}>
+            Administra qué idiomas se usan para personalizar tu experiencia en
+            Twitter.
+          </Paragraph>
+        </List.Item>
+        <List.Item
+          to="/settings/accessibility_display_and_languages"
+          iconLeft={<Icon svg="closedEyes" size="SMALLER" color="SECONDARY" />}
+        >
+          <Paragraph ass="p" type="h5" marginBottom={false}>
+            Uso de datos
+          </Paragraph>
+          <Paragraph color="SECONDARY" ass="p" type="h6" marginBottom={false}>
+            Establece límites para el modo en que Twitter usa algunos de los
+            datos de tu red. Esta configuración afecta a todas las cuentas de
+            Twitter en este navegador.
+          </Paragraph>
+        </List.Item>
       </List>
     </AccessibilityDispLgStyle>
   );
