22582a4fbe9ea7813ab00be18b1c0582500e7977
jerson100 <juamkoo@gmail.com>
2022-05-21 14:17:57 -0500

    Agregando componente tweet a la vista display

--- src/views/private/Settings/AccessibilityDispLg/views/DisplayView/DisplayView.jsx
+++ src/views/private/Settings/AccessibilityDispLg/views/DisplayView/DisplayView.jsx
@@ -1,22 +1,37 @@
 import React from "react";
 import Paragraph from "../../../../../../components/common/Paragraph";
-import Space from "../../../../../../components/common/Space";
+import Separator from "../../../../../../components/common/Separator";
+import Tweet from "../../../../../../components/common/Tweet";
 import { ContentStyle } from "../../../../../../components/styleComponents/content.style";
 import { useChangeMainHeaderLayout } from "../../../../../../hooks/useChangeMainHeaderLayout";
+
+const tweet = {
+  user: {
+    img: "https://pbs.twimg.com/profile_images/1013798240683266048/zRim1x6M_x96.jpg",
+    username: "Twitter",
+    gmail: "@Twitter",
+  },
+  content:
+    "La esencia de Twitter son mensajes breves llamados Tweets, como este, que pueden incluir fotos, videos, enlaces, texto, hashtags y menciones en el formato ",
+  date: "7h",
+  comments: 120,
+  likes: 10,
+  retwitts: 1,
+};
 
 const DisplayView = () => {
   useChangeMainHeaderLayout("Mostrar");
   return (
-    <div>
-      <Space type="SMALL" />
+    <>
       <ContentStyle>
         <Paragraph ass="P" color="SECONDARY" type="h6" marginBottom={false}>
           Administra el fondo, color y tamaño de la fuente. Esta configuración
           afecta a todas las cuentas de Twitter en este navegador.
         </Paragraph>
       </ContentStyle>
-      <Space type="SMALL" />
-    </div>
+      <Tweet {...tweet} />
+      <Separator />
+    </>
   );
 };
 
