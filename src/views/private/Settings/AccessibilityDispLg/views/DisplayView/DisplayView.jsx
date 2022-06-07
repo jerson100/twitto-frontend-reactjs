537b5ee29582405087c2a278fbe015f9a0ea7ad9
jerson100 <juamkoo@gmail.com>
2022-06-06 22:08:06 -0500

    Cambiando los colores

--- src/views/private/Settings/AccessibilityDispLg/views/DisplayView/DisplayView.jsx
+++ src/views/private/Settings/AccessibilityDispLg/views/DisplayView/DisplayView.jsx
@@ -52,7 +52,14 @@ const DisplayView = () => {
           Color
         </Paragraph>
         <SelectColorGroup
-          colors={["red", "yellow", "blue", "purple", "orange", "white"]}
+          colors={[
+            "rgb(29, 155, 240)",
+            "rgb(255, 212, 0)",
+            "rgb(249, 24, 128)",
+            "rgb(120, 86, 255)",
+            "rgb(255, 122, 0)",
+            "rgb(0, 186, 124)",
+          ]}
           selectedItem="red"
         />
       </ContentStyle>
