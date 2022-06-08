9ebc4269477b8254a2e4c253e4348ab258fbdabb
jerson100 <juamkoo@gmail.com>
2022-06-08 07:47:26 -0500

    Creamos nuestra función para obtener el feed del usuario que accede al sistema

--- src/api/tweet.js
+++ src/api/tweet.js
@@ -1,6 +1,10 @@
 import { URL } from "../configs/api";
 import { AuthToken } from "../utils/models/token";
-
+/**
+ * Crea un nuevo tweet
+ * @param description Descripción del tweet
+ * @returns
+ */
 const createTweet = async (description) => {
   const access_token = AuthToken.get();
 
@@ -24,4 +28,27 @@ const createTweet = async (description) => {
   return dataJson.data;
 };
 
-export default { createTweet };
+/**
+ * Nos permite obtener todos los tweets del usuario logueado en el sistema, nos muestra en la linea del tiempo, tanto los tweets del usuario como a los que sigue.
+ * @returns
+ */
+const getFeed = async () => {
+  const access_token = AuthToken.get();
+
+  const data = await fetch(`${URL}/tweets/timeline/feed`, {
+    headers: {
+      authorization: `-Bearer ${access_token}`,
+      "content-type": "application/json",
+    },
+  });
+
+  const dataJson = await data.json();
+
+  if (!dataJson.ok) {
+    throw dataJson.message || "Ocurrió un error al procesar la solicitud";
+  }
+
+  return dataJson.data;
+};
+
+export default { createTweet, getFeed };
