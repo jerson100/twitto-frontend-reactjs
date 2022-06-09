85318484bd193e09c25ab12f2429b2d5c5204c28
jerson100 <juamkoo@gmail.com>
2022-06-08 23:35:35 -0500

    Creamos el hook useTweet para reutilizar en el proyecto

--- src/hooks/useTweet.js
+++ src/hooks/useTweet.js
@@ -1,45 +1,43 @@
-import { useCallback, useEffect, useReducer } from "react"
-import {tweetReducer,TWEET_ACTIONS,TWEET_INITIAL_STATE } from "../reducers/tweetReducer"
-import TweetApi from '../api/tweet'
+import { useCallback, useEffect, useReducer } from "react";
+import {
+  tweetReducer,
+  TWEET_ACTIONS,
+  TWEET_INITIAL_STATE,
+} from "../reducers/tweetReducer";
+import TweetApi from "../api/tweet";
 
 const useTweet = () => {
-    const [tweets, dispatch] = useReducer(tweetReducer, TWEET_INITIAL_STATE);
+  const [data, dispatch] = useReducer(tweetReducer, TWEET_INITIAL_STATE);
 
-    useEffect(() => {
-        const getT = async () => {
-            dispatch({
-                type: TWEET_ACTIONS.TWEET_SET_LOADING,
-                payload: true
-            });
-            try{
-                const data = await TweetApi.getFeed();
-                console.log(data);
-                dispatch({
-                    type: TWEET_ACTIONS.TWEET_SET,
-                    payload: data
-                })    
-            }catch(e){
-                console.log(e)
-            }finally{
-                dispatch({
-                    type: TWEET_ACTIONS.TWEET_SET_LOADING,
-                    payload: false
-                });
-            }
-        }
-        getT();
-    }, [])
-    
+  useEffect(() => {
+    const getT = async () => {
+      dispatch({
+        type: TWEET_ACTIONS.TWEET_SET_LOADING,
+        payload: true,
+      });
+      try {
+        const data = await TweetApi.getFeed();
+        console.log(data);
+        dispatch({
+          type: TWEET_ACTIONS.TWEET_SET,
+          payload: data,
+        });
+      } catch (e) {
+        console.log(e);
+      } finally {
+        dispatch({
+          type: TWEET_ACTIONS.TWEET_SET_LOADING,
+          payload: false,
+        });
+      }
+    };
+    getT();
+  }, []);
 
-    const getTweets = useCallback(
-      () => {
-        
-      },
-      [],
-    )
-    
+  return {
+    loading: data.loading,
+    tweets: data.tweets,
+  };
+};
 
-}
-
-
-export defaukt {useTweet}
+export default { useTweet };
