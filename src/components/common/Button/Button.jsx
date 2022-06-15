2006fe05e06b05137fbc5d1f93cea688bdea0de1
jerson100 <juamkoo@gmail.com>
2022-06-15 13:38:03 -0500

    Modificando el componente Button porque no hay necesidad de tener un componente BaseButton

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { ButtonStyle } from "./button.style";
-import Icon from "../Icon/Icon.jsx";
+import { ButtonStyle, GosthButtonStyle } from "./button.style";
+// import Icon from "../Icon/Icon.jsx";
 
 const Button = ({
   children,
@@ -15,27 +15,64 @@ const Button = ({
   Icon,
   className,
 }) => {
-  return (
-    <ButtonStyle
-      handleClick={handleClick}
-      size={size}
-      block={block}
-      rounded={rounded}
-      type={type}
-      color={color}
-      maxWidth={maxWidth}
-      className={className}
-      isText={children === null}
-    >
-      {Icon && <div className="icon">{Icon}</div>}
-      {children && <span className="text">{children}</span>}
-    </ButtonStyle>
-  );
+  switch (type) {
+    case "DEFAULT":
+      return (
+        <ButtonStyle
+          onClick={handleClick}
+          $size={size}
+          block={block}
+          rounded={rounded}
+          color={color}
+          maxWidth={maxWidth}
+          className={className}
+          isText={children != null || children != undefined}
+        >
+          {Icon && <div className="icon">{Icon}</div>}
+          {children && <span className="text">{children}</span>}
+        </ButtonStyle>
+      );
+    case "GOSTH":
+      return (
+        <GosthButtonStyle
+          onClick={handleClick}
+          $size={size}
+          block={block}
+          rounded={rounded}
+          color={color}
+          maxWidth={maxWidth}
+          className={className}
+          isText={children != null || children != undefined}
+        >
+          {Icon && <div className="icon">{Icon}</div>}
+          {children && <span className="text">{children}</span>}
+        </GosthButtonStyle>
+      );
+  }
+  return null;
+
+  //   console.log(children !== undefined);
+  //   return (
+  // <ButtonStyle
+  //   handleClick={handleClick}
+  //   size={size}
+  //   block={block}
+  //   rounded={rounded}
+  //   type={type}
+  //   color={color}
+  //   maxWidth={maxWidth}
+  //   className={className}
+  //   isText={children != null || children != undefined}
+  // >
+  //   {Icon && <div className="icon">{Icon}</div>}
+  //   {children && <span className="text">{children}</span>}
+  // </ButtonStyle>
+  //   );
 };
 
 Button.propTypes = {
   handleClick: PropTypes.func,
-  size: PropTypes.oneOf(["SMALL", "NORMAL", "BIG", "VERY_BIG"]),
+  size: PropTypes.oneOf(["SMALLER", "SMALL", "NORMAL", "BIG", "VERY_BIG"]),
   block: PropTypes.bool,
   rounded: PropTypes.bool,
   type: PropTypes.oneOf(["DEFAULT", "GOSTH"]),
--- src/components/common/Button/button.style.js
+++ src/components/common/Button/button.style.js
@@ -1,7 +1,115 @@
 import styled, { css } from "styled-components";
 import { BUTTONS } from "../../../configs/style";
-import BaseButton from "../BaseButton";
 import { darken, lighten } from "polished";
+import { SIZES } from "../../../configs/style";
+
+const BUTTONS_SIZE = {
+  [SIZES.SMALL]: {
+    fontSize: "13px",
+    padding: "0 7px",
+    height: 24,
+  },
+  [SIZES.SMALLER]: {
+    fontSize: "14px",
+    padding: "0 11px",
+    height: 28,
+  },
+  [SIZES.NORMAL]: {
+    fontSize: "15px",
+    height: 32,
+    padding: "4px 12px",
+  },
+  [SIZES.BIG]: {
+    fontSize: "16px",
+    padding: "4px 16px",
+    height: 40,
+  },
+  [SIZES.VERY_BIG]: {
+    fontSize: "17px",
+    height: 48,
+    padding: "4px 20px",
+  },
+};
+
+const BaseButtonStyle = styled.button`
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: ${({ rounded }) => (rounded ? "99999px" : "2px")};
+  border: solid 1px rgba(0, 0, 0, 1);
+  cursor: pointer;
+  font-weight: 400;
+  line-height: 1.5;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  outline: solid 2px transparent;
+  transition: transform 200ms ease;
+  ${({ $size }) => css`
+    font-size: ${BUTTONS_SIZE[$size].fontSize};
+    padding: ${BUTTONS_SIZE[$size].padding};
+    height: ${BUTTONS_SIZE[$size].height}px;
+    min-width: ${BUTTONS_SIZE[$size].height}px;
+  `}
+  ${({ maxWidth }) => css`
+    max-width: ${maxWidth};
+    width: 100%;
+  `}
+  &:focus {
+    border-color: ${({ theme }) => theme.COLORS.THIRD};
+    outline-color: ${({ theme }) => theme.COLORS.THIRD};
+  }
+  &:hover {
+    border-color: rgba(255, 255, 255, 0.7);
+  }
+  &:active {
+    border: solid 1px ${({ theme }) => theme.COLORS.THIRD};
+    background-color: rgba(255, 255, 255, 0.8);
+    transform: scale(1.05);
+  }
+  ${({ block }) =>
+    block
+      ? css`
+          display: flex;
+          margin-bottom: 1rem;
+          width: 100%;
+        `
+      : css`
+          display: inline-flex;
+          margin-bottom: 1rem;
+          margin-right: 1rem;
+          width: initial;
+        `}
+  align-items: center;
+  justify-content: center;
+`;
+
+const ButtonStyle = styled(BaseButtonStyle)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${({ isText }) =>
+    !isText &&
+    css`
+      padding: 5px;
+    `}
+  .icon {
+    & > div {
+      ${({ $size }) =>
+        css`
+          width: ${BUTTONS_SIZE[$size].height - 10}px;
+          height: ${BUTTONS_SIZE[$size].height - 10}px;
+        `}
+      ${({ isText }) =>
+        isText &&
+        css`
+          margin-right: 0.2rem;
+        `}
+    }
+  }
+  .text {
+  }
+`;
+
+const GosthButtonStyle = styled(ButtonStyle)``;
 
 const getStyleButton = ({ type, theme, color }) => {
   let c, b, bg, bg_hover, b_hover;
@@ -31,9 +139,6 @@ const getStyleButton = ({ type, theme, color }) => {
     background-color: ${bg};
     border-color: ${b};
     color: ${c};
-    /* &:focus {
-      outline: solid 2px ${bg_hover};
-    } */
     &:hover {
       background-color: ${bg_hover};
       border-color: ${b_hover};
@@ -50,26 +155,29 @@ const getStyleButton = ({ type, theme, color }) => {
   `;
 };
 
-const ButtonStyle = styled(BaseButton)`
-  ${(res) => getStyleButton(res)}
-  ${({ isText }) =>
-    !isText &&
-    css`
-      line-height: 0;
-    `}
-  .text {
-  }
-  .icon {
-    display: inline-block;
-    margin-right: 0;
-    ${({ isText }) =>
-      !isText &&
-      css`
-        margin-right: 0;
-      `}
-  }
-`;
+// const ButtonStyle = styled(BaseButton)`
+//   ${(res) => getStyleButton(res)}
+//   ${({ isText }) =>
+//     !isText &&
+//     css`
+//       line-height: 0;
+//       padding: 4px;
+//     `}
+//   .text {
+//   }
+//   .icon {
+//     ${({ isText }) =>
+//       isText &&
+//       css`
+//         margin-right: 0.2rem;
+//       `}
+//     & > div {
+//       width: 14px;
+//       height: 14px;
+//     }
+//   }
+// `;
 
 // const ButtonGosht = styled(ButtonStyle)``;
 
-export { ButtonStyle };
+export { ButtonStyle, GosthButtonStyle };
